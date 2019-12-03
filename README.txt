8881f52210117f9c97f39cc0b182899260f59c40
Oleg Kostyuchenko <olegskostyuchenko@gmail.com>
2019-12-04 00:09:22 +0300

    Commit 03122019_0

--- README.txt
+++ README.txt
@@ -8,3 +8,5 @@
 Проект подготовлен к работе по повторению White Belt.
 
 
+Commit 03122019_0
+Counting quantity of given symbol in the given word.
--- src/Repeating_wb.cpp
+++ src/Repeating_wb.cpp
@@ -7,9 +7,24 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-
+	string word;
+	char ch;
+	while (1){
+		int qty = 0;
+		cin >> word;
+		if (word == "exit"){
+			break;
+		} else {
+			cin >> ch;
+			qty = count(begin(word), end(word), ch);
+				}
+		cout << qty << endl;
+	}
 	return 0;
 }
