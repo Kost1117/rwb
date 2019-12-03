08047749f5195a78a37017231533e2bc288ce090
Oleg Kostyuchenko <olegskostyuchenko@gmail.com>
2019-12-04 01:21:53 +0300

    count_sort

--- src/Repeating_wb.cpp
+++ src/Repeating_wb.cpp
@@ -7,9 +7,13 @@
 //============================================================================
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-
+	vector<int> nums = {1, 2, 5, 9, 5, 8};
+	for (int n : nums) {
+		cout << n << ',';
+	}
 	return 0;
 }
